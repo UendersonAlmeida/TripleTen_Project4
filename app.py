3084e48b1f0c43ed9c3047c422e3a24c275e71fc
UendersonAlmeida <151178889+UendersonAlmeida@users.noreply.github.com>
2024-05-05 14:58:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import streamlit as st
 car_data = pd.read_csv('vehicles.csv')  # lendo os dados
 
 # Cabeçalho
-st.header('Encontre seu carro .NET')
-
+st.header('Visualização de alguns dados referente ao anúncio de vendas de carros')
+st.header('Aqui você verá três tipos de graficos')
 
  # criar um botão
-hist_button = st.button('Criar Grafico - odometer')
+hist_button = st.button('Criar Gráfico - odometer')
 scatter_button = st.button('Criar gráfico de dispersão')
-
+bar_button = st.button('Criar um gráfico de Barra')
 
 if hist_button:  # se o botão for clicado
     # escrever uma mensagem
@@ -23,12 +23,23 @@ if hist_button:  # se o botão for clicado
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
-if scatter_button:
+if scatter_button: # se o botão for clicado
     # escrever uma mensagem
     st.write('Criar um grafico de disperção para o conjunto de dados de anúncios de vendas de carros')
 
-    # criar um grafico de disperção
+    # criar um gráfico de disperção
     fig = px.scatter(car_data, x="model_year", y="odometer")
 
-    #exibir um grafico Plotly interativo
+    # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
+
+if bar_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um gráfico de barra para o conjunto de dados de anúncios de vendas de carros')
+
+    # criar um gráfico de barras
+    fig1 = px.bar(car_data, x='paint_color', template='none', color='paint_color')
+    fig1.update_layout(title='Contagem de Veículos por Cor', autosize=false, width=500, height=500)
+
+    # exibir um gráfico ploply interativo
+    st.plotly_chart(fig1, use_container_width=True)
